6d9abc8c1cf5b2bed6d04981a0b228a5908d7560
marianexyx <mariusz.pak.89@gmail.com>
2018-06-11 00:37:22 +0200

    Upgrading css in progress

--- game.php
+++ game.php
@@ -3,13 +3,6 @@ checkUser($_SESSION['id']); //sprawdzmay funkcją czy gracz jest zalogowany. fun
 //todo: ogarnąć to w innych plikach
 $user = getUser($_SESSION['id']); //przypisanie do zmiennej user funkcji getuser, która wyciąga dane gracza aktualnie zalogowanego
 
-echo '<div align="right" style="float:right; padding-right: 50px"> <b>Użytkownik:</b> '.$user['login'].'&nbsp;&nbsp; </div>';
+echo '<div align="center" style="float:left; margin:0 auto;"> <b>Użytkownik:</b> '.$user['login'].'&nbsp;&nbsp; </div>';
 
-/*<table align="center">
-	<tr>
-		<td><b>Użytkownik:</b></td>
-		<td style="padding: 10px">'.$user['login'].'&nbsp;&nbsp;
-		</td>
-	</tr>
-</table>';*/
 ?>
--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 		<title>Budgames- Szachy</title>
 		
+		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<link rel="stylesheet" type="text/css" href="css/dialogNoClose.css">
 		<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css"> 
 		
@@ -18,42 +19,43 @@
 		<script src="functions.js"></script>
 	</head>
 	<body>	
-		<table  align="center"  cellspacing="5" cellpadding="5" width="900" border="1">
-			<tr align="center">
-				<td colspan="3">
-					<?
-						ob_start();
-						session_start(); 
-						require_once('include/inc.php');
-						require_once('disabling.php');
-						
-						error_reporting( error_reporting() & ~E_NOTICE ); //wyłącz ostrzeżenie, że niezdefiniowana jest zmienna 'a' i inne tego typu
-						
-						if(empty($_SESSION['id'])) 
-							echo '<div align="center" style="float:left; padding-left: 50px"> <a href="index.php?a=register">Zarejestruj się</a> | <a href="index.php?a=login">Zaloguj się</a> </div>';
-						else 
-							echo '<div id="info" style="float:left; padding-left: 50px"><a href="#" onClick="return info();">Kontakt</a> | 
-								  <a href="index.php?a=logout" onclick="return confirmLogout();">Wyloguj się</a></div>'; 
-						
-						switch($_GET['a']) //zmienna w pasku ustalająca stronę po ob_end_flush; 'a' pobierane z "a href'ów"
-						{
-							case 'home': require_once('home.php'); break; //domyślny ( w takim razie po co tutaj?)
-							case 'login': require_once('login.php'); break;
-							case 'register': require_once('register.php'); break;
-							case 'game': require_once('game.php'); break;
-							case 'logout':
-							$_SESSION = array(); // czyszczenie sesji
-							session_destroy(); // niszczenie sesji. resetuje się na nowe //todo: czy msie rozni od session_unset?
-							header("Location: index.php"); //header przenosi na stronę główną
-							break;
-							default: require_once('home.php'); break;
-						}
-						ob_end_flush(); // wyrzygaj stronę
+		<div align="center" id="mainDiv">
+			<div id ="menu">
+				<?
+					ob_start();
+					session_start(); 
+					require_once('include/inc.php');
+					require_once('disabling.php');
+					
+					error_reporting( error_reporting() & ~E_NOTICE ); //wyłącz ostrzeżenie, że niezdefiniowana jest zmienna 'a' i inne tego typu
+					
+					if(empty($_SESSION['id'])) 
+						echo '<div align="center" style="float:left; margin:0 auto;"> 
+								<a href="index.php?a=register">Zarejestruj się</a> | <a href="index.php?a=login">Zaloguj się</a> 
+							  </div>';
+					else 
+						echo '<div id="info" align="center" style="float:left; margin:0 auto;">
+								<a href="#" onClick="return info();">Kontakt</a> | <a href="index.php?a=logout" onclick="return confirmLogout();">Wyloguj się</a>
+							  </div>'; 
+					
+					switch($_GET['a']) //zmienna w pasku ustalająca stronę po ob_end_flush; 'a' pobierane z "a href'ów"
+					{
+						case 'home': require_once('home.php'); break; //domyślny ( w takim razie po co tutaj?)
+						case 'login': require_once('login.php'); break;
+						case 'register': require_once('register.php'); break;
+						case 'game': require_once('game.php'); break;
+						case 'logout':
+						$_SESSION = array(); // czyszczenie sesji
+						session_destroy(); // niszczenie sesji. resetuje się na nowe //todo: czy msie rozni od session_unset?
+						header("Location: index.php"); //header przenosi na stronę główną
+						break;
+						default: require_once('home.php'); break;
+					}
+					ob_end_flush(); // wyrzygaj stronę
 					?>
-				</td>
-			</tr>
-			<tr>
-				<td align="center" valign="top"> <!-- todo: dlaczego wogle ten cały kod php/js poniżej jest w tagach <td>? -->
+			</div>
+			<div id="content">
+				<div id="game"><!-- todo: dlaczego wogle ten cały kod php/js poniżej jest w indexie? -->
 					<? 
 						$user = getUser($_SESSION['id']);
 						/*$_SESSION['table_id'] = 1; //póki co jest tylko jeden stół, więc zmienna zbędna*/
@@ -155,73 +157,72 @@
 					</script> 	
 					
 					<div id="video">
-						<iframe id="ytplayer" type="text/html" width="720" height="480"
-						  src="https://www.youtube.com/embed/live_stream?channel=UCLVBCJh3oKqWR2qo58BVd-w&autoplay=1&enablejsapi=1&origin=http://example.com"></iframe>
+						<iframe id="ytplayer" type="text/html" width="854" height="480"
+						  src="https://www.youtube.com/embed/live_stream?channel=UCLVBCJh3oKqWR2qo58BVd-w&autoplay=1&enablejsapi=1&origin=http://example.com">
+						  </iframe>
 					</div>
-					<br/>
-					<br/>
-					<div id="clientPTE" style="float:none">
-						<textarea readonly id="clientPlainTextWindow" style="width:400px;height:170px;"></textarea>
+					<div id="table" align="center">
+						<div id="playersBoxes">
+							<div id="whitePlayerBox">
+								<div id="whiteTime"><br/>30:00</div>
+								<div id="whitePlayerSign"><font size="10">&#9817;</font></div>
+								<div id="whitePlayerBtns">
+									Gracz Biały<br/>
+									<button id="whitePlayer" onClick="clickedBtn('sitOnWhite')" disabled>Loading...</button> 
+									<button id="standUpWhite" onClick="clickedBtn('standUp')" disabled>Wstań</button> 
+								</div>
+							</div> 
+							<div id="resign">
+								<div id="giveUpDialog" hidden="hidden">Czy chcesz opuścić grę?</div> <!-- czy to tu musi być? -->
+								<br/><button id="giveUpBtn" onClick="giveUp()" disabled>zrezygnuj</button>
+							</div>
+							<div id="blackPlayerBox">
+								<div id="blackTime" align="center"><br/>30:00</div>
+								<div id="blackPlayerSign"><font size="10">&#9823;</font></div> <!--css-->
+								<div id="blackPlayerBtns">
+									Gracz Czarny<br/>
+									<button id="blackPlayer" onClick="clickedBtn('sitOnBlack')" disabled>Loading...</button> 
+									<button id="standUpBlack" onClick="clickedBtn('standUp')" disabled>Wstań</button> 
+								</div>	
+							</div>
+							<div style="clear:both"></div>
+						</div>
+						<div id="moveSection" align="center">
+							Przemieść bierkę z&nbsp;&nbsp;
+							<input type="text" id="pieceFrom" name="pieceFrom" maxlength="2" size="2" disabled />&nbsp;na&nbsp; 
+							<input type="text" id="pieceTo"   name="pieceTo"   maxlength="2" size="2" disabled />&nbsp;&nbsp;
+							<button id="movePieceButton" onClick="movePiece();" disabled >Wyślij</button> 
+						</div>
+					</div>
+				</div>  
+				<div style=" float: left;">
+					<div id="clientPTE">
+						<textarea readonly id="clientPlainTextWindow"></textarea>
 					</div>	
-					
-					<div id="promotionContent" style="float:left"></div>
+					<div id="promotionContent" style="float:left"></div> <!-- todo: czy to możę być gdzie indziej? i czy musi mieć float.left?-->
 					<div style="clear: both;">
 						<button id="infoPTE" onClick="changePTEsource('infoPTE')" disabled>informacje</button> 
 						<button id="historyPTE" onClick="changePTEsource('historyPTE')">historia</button> 
 						<button id="queuePTE" onClick="changePTEsource('queuePTE')">kolejka</button> 
-						&nbsp;&nbsp;<button id="queuePlayer" onClick="clickedBtn('queueMe')" style="display: none;" disabled>Zakolejkuj</button>
-					    <button id="leaveQueue" onClick="clickedBtn('leaveQueue')" style="display: none;" disabled>Opuść</button>
+						&nbsp;&nbsp;<button id="queuePlayer" onClick="clickedBtn('queueMe')" disabled>Zakolejkuj</button>
+						<button id="leaveQueue" onClick="clickedBtn('leaveQueue')" disabled>Opuść</button>
 					</div>
 					<div id="promoteDialog"> </div> <!-- bez tego nie chce mi działać dialog-promote-->
 					<div id="startGameDialog" hidden="hidden">Wciśnij start, by rozpocząć grę. Pozostały czas: 120</div> 
 					<div id="endOfGameDialog" hidden="hidden">Koniec gry.</div> 
-					
-				</td>  
-				<td align="center" valign="top">
-					<table width="100%" cellpadding="15">
-						<div id="whitePlayerBox" style="float:left; padding: 5px">
-							<div style="float:left"><font size="10">&#9817;</font></div>
-							<div style="float:left; padding-top: 10px">
-								Gracz Biały<br/>
-								<button id="whitePlayer" onClick="clickedBtn('sitOnWhite')" style="min-width:60px;" disabled >Loading...</button> 
-								<button id="standUpWhite" onClick="clickedBtn('standUp')" disabled>Wstań</button> 
-							</div>
-						</div> 
-						<div id="blackPlayerBox" style="float:right; padding: 5px">
-							<div style="float:left"><font size="10">&#9823;</font></div>
-							<div style="float:left; padding-top: 10px" >
-								Gracz Czarny<br/>
-								<button id="blackPlayer" onClick="clickedBtn('sitOnBlack')" style="min-width:60px;" disabled>Loading...</button> 
-								<button id="standUpBlack" onClick="clickedBtn('standUp')" disabled>Wstań</button> 
-							</div>	
-						</div>
-					</table>
-					<table width="80%" cellpadding="15">
-						<td align="center">
-							<div id="whiteTime" style="float:left">30:00</div>
-							
-							<div id="giveUpDialog" hidden="hidden">Czy chcesz opuścić grę?</div>
-							<button id="giveUpBtn" onClick="giveUp()" disabled>zrezygnuj</button>
-							<div id="blackTime" style="float:right">30:00</div>
-						</td>
-					</table> 
-					<div id="moveSection" style="padding: 5px">
-						Przemieść bierkę z&nbsp;&nbsp;
-						<input type="text" id="pieceFrom" name="pieceFrom" maxlength="2" size="2" disabled />&nbsp;na&nbsp; 
-						<input type="text" id="pieceTo"   name="pieceTo"   maxlength="2" size="2" disabled />&nbsp;&nbsp;
-						<button id="movePieceButton" onClick="movePiece();" disabled >Wyślij</button> 
+					<div align="center"> 
+						<iframe width="350px" height="500px" src="https://www.youtube.com/live_chat?v=i6EPyaxc-GI&embed_domain=budgames.pl">
+						</iframe>
 					</div>
-					 <iframe width="350px" height="500px" 
-					src="https://www.youtube.com/live_chat?v=i6EPyaxc-GI&embed_domain=budgames.pl"></iframe>
-				</td>
-			</tr>
-		</table>  
+				</div>
+			</div>
+			<div id="footage" align="center">
+				<font size="2">
+					<a href="http://cosinekitty.com/chenard/">Chess engine</a> by <a href="http://cosinekitty.com/">Don Cross</a>
+				</font>
+			</div>
+		</div>  
 		
-		<p align="center">
-			<font size="2">
-				<a href="http://cosinekitty.com/chenard/">Chess engine</a> by <a href="http://cosinekitty.com/">Don Cross</a>
-			</font>
-		</p>
 		
 		<script> //todo: zamknąć to i przenieść poza index
 			document.getElementById("pieceFrom").onkeyup = function() {pieceFromOnKeyPress()};
